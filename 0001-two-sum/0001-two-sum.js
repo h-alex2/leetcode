6e9851e9886d754a933d53f7bcf66d6a72d16461
임현정 | Alex Im <glowhyun1@gmail.com>
2023-07-06 18:00:03 +0900

    Time: 77 ms (56.91%), Space: 44.3 MB (13.84%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const store = {};
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (store[target - nums[i]]) {
+            return [store[target - nums[i]][0], i];
+        }
+        
+        if (store[nums[i]]) {
+            store[nums[i]].push(i);
+        } else {
+            store[nums[i]] = [i];
+        }
+    }
+};
