4c0286d8d6c7096f0dbc93059f489e825f048d95
임현정 | Alex Im <glowhyun1@gmail.com>
2023-08-10 23:50:32 +0900

    Time: 57 ms (91.15%), Space: 45.6 MB (31.31%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+var maxDepth = function(tree, count = 0) {
+    if (!tree) return count;
+
+    return Math.max(maxDepth(tree.left, count + 1), maxDepth(tree.right, count + 1));
+};
